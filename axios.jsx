b6a814282c74789e7ae1f10611dfa64331b95d84
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-08-31 01:27:46 +0300

    Update axios.jsx

--- axios.jsx
+++ axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://127.0.0.1:8000/';
+const baseUrl = 'https://fantasiaapi.onrender.com/';
 
 const axiosInstance = axios.create({
     baseURL: baseUrl,
