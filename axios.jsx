d1ff3bac66fcca12cea3895ba6c03cba54887b5e
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-09-06 12:44:26 +0300

    Update axios.jsx

--- axios.jsx
+++ axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://fotistouman.pythonanywhere.com/users/';
+const baseUrl = 'https://fotistouman.pythonanywhere.com/users';
 
 const axiosInstance = axios.create({
     baseURL: baseUrl,
