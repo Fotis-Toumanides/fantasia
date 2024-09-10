58c3f838f212cfec9b5e30c3fef813df88544a80
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-09-10 13:01:05 +0300

    Update axios.jsx

--- axios.jsx
+++ axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = '/home/FotisTouman/fantasiaapi/users/';
+const baseUrl = '/home/FotisTouman/fantasiaapi/';
 
 const axiosInstance = axios.create({
     baseURL: baseUrl,
