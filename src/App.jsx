6aba257d20e9a6fee4b2963061af56e39961b9b9
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-09-12 14:57:49 +0300

    Update App.jsx backend moved to render

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
   useEffect(() => {
     async function fetchBooks(){
       try{
-        const response = await axios.get('https://fotistouman.pythonanywhere.com/bookdescriptions');
+        const response = await axios.get('https://fantasiaapi.onrender.com/bookdescriptions');
         setBookDescriptions(response.data);
       } catch(error){
         console.error('Error fetching the books:', error);
