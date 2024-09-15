904a7df8459b02c2d064799f0071b03e182288b2
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-09-15 04:33:29 +0300

    Update BookDetails.jsx

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -58,6 +58,7 @@ export default function BookDetails({bookDetails=[]}) {
 
     return(
         <>
+        {!bookDetails && <p>Loading...</p> }
         {bookDetails &&    
         <ul className='list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
         {bookDetails.map((bookDetail) => (
